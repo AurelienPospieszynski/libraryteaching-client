bd4b00193c2e51916c49b09ba04b8ea9bfd801ab
maxime <maxime.pospieszynski@gmail.com>
2024-03-27 17:30:41 +0100

    add buttons (WIP)

--- src/app/components/accueil/book/book.component.ts
+++ src/app/components/accueil/book/book.component.ts
@@ -3,30 +3,33 @@ import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../../services/book/book.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
+  imports: [CommonModule, FormsModule],
   providers: [BookService],
   templateUrl: './book.component.html',
 })
 export class BookComponent implements OnInit {
   books: Book[] = [];
+  filteredBooks: Book[] = [];
   showBookList: boolean = false;
+  searchTerm: string = '';
 
   constructor(private bookService: BookService) {}
 
   async ngOnInit() {
     this.books = await this.bookService.getAllBooks();
+    this.filteredBooks = this.books;
   }
 
   toggleBookList() {
     this.showBookList = !this.showBookList;
+    if (!this.showBookList) {
+      this.searchTerm = '';
+    }
   }
 
   trackByBookId(index: number, book: Book): number {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
     important: true,
     content: ["./src/**/*.{html,css,ts}"],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          fontOrbitron: ["Orbitron", "sans-serif"],
+          fontMontserrat: ["Montserrat", "sans-serif"],
+        },
+        colors: {
+          colorBlue: "#0EC4ED",
+        },
+      },
     },
     variants: {
       extend: {},
